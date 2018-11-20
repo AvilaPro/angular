0cf201132d41619c5428f99aee1ecf160a79ef39
Eduardo Mejias <mejiasavilaeduardo@gmailcom>
2018-11-20 00:12:27 -0400

    reagrupado de clase 1 y desafio 1 de angular

---
+++ Desafio 1 Angular/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'desafio1';
+  nombre = 'Eduardo';
+  cedula = '19.591.303';
+  correo = 'mejiasavilaeduardo@gmail.com';
+  nota_promedio_desafios = 100;
+  nota_examen = 100;
+}
